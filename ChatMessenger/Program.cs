65c114ec49c16405ecbb963f1cd088ca126e2939
evripidis katsianos <euripidiskatsianos@gmail.com>
2019-01-01 17:44:12 +0200

    maskpassword

--- ChatMessenger/Program.cs
+++ ChatMessenger/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Dapper;
 using System.Data;
 using System.Data.SqlClient;
+using System.Security;
 
 namespace ChatMessenger
 {
@@ -15,16 +16,20 @@ namespace ChatMessenger
 
         static void Main(string[] args)
         {
-            Method1();
-            Console.WriteLine("login commit2");
+            LoginMethod();
         }
 
-        static void Method1()
+
+
+        static void LoginMethod()
         {
+            bool found = false;
+
             Console.WriteLine("Login Screen");
+            Console.WriteLine("\n");
             Console.Write("Username: ");
             string username = Console.ReadLine();
-
+            Console.WriteLine("\n");
 
             SqlConnection dbcon = new SqlConnection(connectionString);
             using (dbcon)
@@ -32,29 +37,81 @@ namespace ChatMessenger
                 dbcon.Open();
                 var user = dbcon.Query("select * from users");
 
-                foreach (var u in user)
+                while (found == false)
                 {
-                    if (username == u.username)
+                    foreach (var u in user)
                     {
-                        Console.Write("Password: ");
-                        string password = Console.ReadLine();
-                        while (password != u.pass)
+                        if (username == u.username)
                         {
-                            Console.WriteLine("Incorrect password try again");
-                            Console.Write("Password: ");
-                            password = Console.ReadLine();
+                            string correctPassword = u.pass;
+                            found = PasswordMethod(correctPassword);
+                            Console.WriteLine("you are enter in");
+                            ApplicationMethod();
                         }
-                        Console.WriteLine("you are enter in");
                     }
-                    else
+                    if (found == false)
                     {
                         Console.WriteLine("The user doesn't exists");
+                        Console.Write("Username: ");
+                        username = Console.ReadLine();
+                        Console.WriteLine("\n");
                     }
-                    Console.WriteLine($"Username: {u.username}, Password: {u.pass}");
                 }
-
-
             }
+        }
+
+
+        static bool PasswordMethod(string correctPassword)
+        {
+            Console.WriteLine("maskpassword");
+            bool check = false;
+            string password = ""; 
+            ConsoleKeyInfo keyInfo;
+
+            while (check == false)
+            {
+                Console.Write("Password: ");
+                do
+                {
+                    keyInfo = Console.ReadKey(true);
+                    // Skip if Backspace or Enter is Pressed
+                    if (keyInfo.Key != ConsoleKey.Backspace && keyInfo.Key != ConsoleKey.Enter)
+                    {
+                        password += keyInfo.KeyChar;
+                        Console.Write("*");
+                    }
+                    else if (keyInfo.Key == ConsoleKey.Backspace && password.Length > 0)
+                    {
+                        // Remove last charcter if Backspace is Pressed
+                        password = password.Substring(0, (password.Length - 1));
+                        Console.Write("\b \b");
+                    }
+                }
+                while (keyInfo.Key != ConsoleKey.Enter);
+                Console.WriteLine("\n");
+                if (password != correctPassword)
+                {
+                    Console.WriteLine("Incorrect password try again");
+                    password = "";
+                }
+                else
+                {
+                    check = true;
+                }
+            }
+            Console.WriteLine("end");
+            return check;
+        }
+
+        static void DatabaseMethod()
+        {
+
+        }
+        
+
+        static void ApplicationMethod()
+        {
+
         }
     }
 }
