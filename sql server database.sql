9989da0156deedef8631e6e156f6e9cb3b0792b3
evripidis katsianos <euripidiskatsianos@gmail.com>
2019-01-17 08:57:26 +0200

    Revert "Revert "Update sql server database.sql""
    This reverts commit 896b62f81f6a3b937b1d61ffc45c24092c3ac61d.

--- sql server database.sql
+++ sql server database.sql
@@ -1,5 +1,6 @@
 
 CREATE DATABASE ChatDB;
+GO
 
 USE ChatDB;
 
@@ -146,13 +147,7 @@ BEGIN
 	WHERE username = @username AND pass = CONVERT(VARCHAR(MAX),HASHBYTES('MD5', CAST(salt AS VARCHAR(MAX)) + @pass) ,2)
 END
 
-									     
-									     
-									     
-									     
-DECLARE @user VARCHAR(100) 
-EXECUTE check_Password  @username = 'admin', @pass = 'dmin' , @user = @user OUTPUT
-SELECT @user 
+
 
 									     
 									     
